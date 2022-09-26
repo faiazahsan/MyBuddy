05bbbb1a3e2c09d6d745e23213e99847c3a90911
faiazahsan <faiazahsanaks@gmail.com>
2022-09-26 16:56:44 -0400

    buddyName changed

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -21,7 +21,7 @@ public class AddressBook{
 
 
     public static void main(String [] args){
-        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        BuddyInfo buddy = new BuddyInfo("Chris", "Carleton", "613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(0);
